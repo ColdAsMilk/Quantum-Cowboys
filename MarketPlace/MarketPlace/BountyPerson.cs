00b0a9efcc20905bcb5bd29afc6e9e1007dfe487
cook2298 <45172938+cook2298@users.noreply.github.com>
2019-11-06 11:11:04 -0600

    BountyPerson Class

---
+++ MarketPlace/MarketPlace/BountyPerson.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace
+{
+    class BountyPerson
+    {
+        private string name;
+        private string wantedLevel;
+        private double award;
+        private string location;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string WantedLevel
+        {
+            get
+            {
+                return wantedLevel;
+            }
+            set
+            {
+                wantedLevel = value;
+            }
+        }
+        public double Award
+        {
+            get
+            {
+                return award;
+            }
+            set
+            {
+                location = value;
+            }
+        }
+        public string Location
+        {
+            get
+            {
+                return location;
+            }
+            set
+            {
+                location = value;
+            }
+        }
+        public BountyPerson()
+        {
+            name = "Billy Bob";
+            wantedLevel = "Dead or Alive";
+            award = 500.00;
+            location = "Texas";
+        }
+        public BountyPerson(string na, string wl, double aw, string lo)
+        {
+            Name = na;
+            WantedLevel = wl;
+            Award = aw;
+            Location = lo;
+        }
+    }
+}
