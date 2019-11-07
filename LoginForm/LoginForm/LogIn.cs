b393c14905e02ff51fbf9ca9680334bb133c03c8
ColdAsMilk <32677787+ColdAsMilk@users.noreply.github.com>
2019-11-07 08:12:35 -0600

    Registration Working

--- LoginForm/LoginForm/LogIn.cs
+++ LoginForm/LoginForm/LogIn.cs
@@ -19,20 +19,21 @@ namespace LoginForm
 
         private void Button1_Click(object sender, EventArgs e)
         {
+            //Creating instance of database, connecting to it and taking the username and password values
             SqlConnection sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\kyler\Documents\GitHub\Quantum-Cowboys\LoginForm\LoginForm\Database1.mdf;Integrated Security=True");
             string query = "Select * from LoginInfo Where username = '" + txtUsername.Text.Trim() + "' and password ='" + txtPassword.Text.Trim() + "'";
             SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
             DataTable dtbl = new DataTable();
             sda.Fill(dtbl);
-            if (dtbl.Rows.Count==1)
+            if (dtbl.Rows.Count==1) //Checking to see if the values are in the database
             {
-                MarketPlace objFrmMain = new MarketPlace();
+                MarketPlace objFrmMain = new MarketPlace(); //Accepts them and lets you through
                 this.Hide();
                 objFrmMain.Show();
             }
             else
             {
-                MessageBox.Show("Check your username and password");
+                MessageBox.Show("Check your username and password"); //Does not accept values
             }
         }
 
---
+++ LoginForm/SQLQuery2.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROC UserAdd
+@UserID int,
+@UserName varchar(50),
+@Password varchar(50)
+AS
+	INSERT INTO LoginInfo(UserID, UserName, Password)
+	VALUES(@UserID, @UserName, @Password)
